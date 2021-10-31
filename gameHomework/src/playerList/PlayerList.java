718d46d14dffe8cf8f324656090880752df7654e
Ahmetatakanokutan <aatakants611@gmail.com>
2021-10-31 11:43:07 +0300

    bitis

---
+++ gameHomework/src/playerList/PlayerList.java
@@ -0,0 +1,11 @@
+package playerList;
+
+public interface PlayerList {
+
+	
+	String id[] = {"1","4","3","65"};
+	String names[] = {"atakan","mahmut","zuhtu","abdi"};
+	String surNames[] = {"okutan","tuncer","sever","adiguzel"};
+	String nationalityId[] = {"43242654","87685456","242423654","9877564345"};
+	String dateOfBirth[] = {"02.05.2002","05.07.1965","03.10.2000","21.05.1980"};
+}
